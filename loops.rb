a8b7f24694b0fc24a39a779d699418d3298f3d06
Micah Shute <micah.shute@gmail.com>
2019-10-15 05:10:13 +0000

    Automatically backed up by Learn

--- loops.rb
+++ loops.rb
@@ -1,7 +1,12 @@
 question = "Who was the only US President to serve two nonconsecutive terms?"
 correct_answer = "Grover Cleveland"
-
+puts question
 5.times do 
-  puts question
   answer = gets.strip
+  if answer == correct_answer
+    puts "That is correct!"
+    break
+  else
+    puts "Sorry, please try again."  
+  end
 end
